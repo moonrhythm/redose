09409332efeaf00e7439e441be55d087c504c953
Thanatat Tamtan <acoshift@gmail.com>
2020-02-16 21:32:01 +0700

    fix scan pattern

--- redose.go
+++ redose.go
@@ -538,7 +538,10 @@ func (h *Server) Scan(conn redcon.Conn, cmd redcon.Command) {
 	}
 
 	pattern, _ := getParam(cmd.Args, "match")
-	pattern = h.sessionKey(conn, pattern) + "*"
+	if pattern == "" {
+		pattern = "*"
+	}
+	pattern = h.sessionKey(conn, pattern)
 
 	count, ok, err := getParamInt64(cmd.Args, "count")
 	if err != nil {
